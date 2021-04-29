558a73bbfcde7d26020103bbe6bc25131fec0741
vitekspeedcuber <shipitsin@phystech.edu>
2021-04-29 15:48:29 +0300

    adaptive layer refactoring

--- models/adaptive_layer.py
+++ models/adaptive_layer.py
@@ -17,33 +17,37 @@ class AdaptiveLayer(torch.nn.Module):
         elif self.adjustment == 'phase':
             torch.nn.init.zeros_(self.frequency_filter)
 
-    def spectrum_adjustment(self, rfft_tensor, weights):
-        init_spectrum = torch.sqrt(torch.pow(rfft_tensor.real, 2) + torch.pow(rfft_tensor.imag, 2))
-        if self.adjustment == 'spectrum':
-            return torch.where(init_spectrum == torch.tensor(0.0), rfft_tensor, weights * rfft_tensor)
-        elif self.adjustment == 'spectrum_log':
-            return torch.where(init_spectrum == torch.tensor(0.0),
-                               rfft_tensor,
-                               (rfft_tensor / (init_spectrum + 1e-16)) * (torch.exp(weights * torch.log(1 + init_spectrum)) - 1))
+    @staticmethod
+    def spectrum_adjustment(rft_tensor, weights):
+        return weights * rft_tensor
 
-    def phase_rotation(self, rfft_tensor, weights):
-        return torch.complex(real=torch.cos(weights) * rfft_tensor.real - torch.sin(weights) * rfft_tensor.imag,
-                             imag=torch.sin(weights) * rfft_tensor.real + torch.cos(weights) * rfft_tensor.imag)
+    @staticmethod
+    def spectrum_log_adjustment(rft_tensor, weights):
+        spectrum = torch.sqrt(torch.pow(rft_tensor.real, 2) + torch.pow(rft_tensor.imag, 2))
+        return torch.where(spectrum == torch.tensor(0.0),
+                           rft_tensor,
+                           (rft_tensor / (spectrum + 1e-16)) * (torch.exp(weights * torch.log(1 + spectrum)) - 1))
 
-    def phase_adjustment(self, rfft_tensor, weights):
-        init_spectrum = torch.pow(rfft_tensor.real, 2) + torch.pow(rfft_tensor.imag, 2)
-        return torch.where(init_spectrum == torch.tensor(0.0), rfft_tensor, self.phase_rotation(rfft_tensor, weights))
+    @staticmethod
+    def phase_adjustment(rft_tensor, weights):
+        return torch.complex(real=torch.cos(weights) * rft_tensor.real - torch.sin(weights) * rft_tensor.imag,
+                             imag=torch.sin(weights) * rft_tensor.real + torch.cos(weights) * rft_tensor.imag)
 
     def forward(self, x):
         weights_size = (x.shape[0],) + tuple(1 for _ in range(len(self.size)))
         transformed_dimensions = tuple([dim for dim in range(1, len(weights_size))])
-        rfft_x = torch.fft.rfftn(x, dim=transformed_dimensions)
 
+        rft_x = torch.fft.rfftn(x, dim=transformed_dimensions)
         if self.adjustment in ('spectrum', 'spectrum_log'):
             w = torch.nn.ReLU()(self.frequency_filter.repeat(weights_size).to(x.device))
-            return torch.fft.irfftn(self.spectrum_adjustment(rfft_x, w), dim=transformed_dimensions)
+            if self.adjustment == 'spectrum':
+                adjusted_rft_x = self.spectrum_adjustment(rft_x, w)
+            elif self.adjustment == 'spectrum_log':
+                adjusted_rft_x = self.spectrum_log_adjustment(rft_x, w)
         elif self.adjustment == 'phase':
             w = torch.clamp(self.frequency_filter.repeat(weights_size).to(x.device),
                             min=0,
                             max=2 * torch.acos(torch.Tensor([-1])).item())
-            return torch.fft.irfftn(self.phase_adjustment(rfft_x, w), dim=transformed_dimensions)
+            adjusted_rft_x = self.phase_adjustment(rft_x, w)
+
+        return torch.fft.irfftn(adjusted_rft_x, dim=transformed_dimensions)
